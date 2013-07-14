9c5893181f9ed89944de261f9b35b7c0fa362656
espeyer <erik.speyer@gmail.com>
2013-07-14 12:16:03 -0400

    js shit

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php include("header.php"); ?>
+<div id="slides">
+    <img src="http://placehold.it/940x528">
+    <img src="http://placehold.it/940x528">
+    <img src="http://placehold.it/940x528">
+    <img src="http://placehold.it/940x528">
+    <img src="http://placehold.it/940x528">
+  </div>
 <?php include("footer.php"); ?>
